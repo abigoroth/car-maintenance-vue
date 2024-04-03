8dca73ac369789ddf08b87a29b4394ba7baf0c98
yakob ubaidi <yakob.ubaidi@gmail.com>
2024-04-04 01:30:26 +0800

    added eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
+      typescript: {},
       'eslint-import-resolver-custom-alias': {
         alias: {
           '@': './app/javascript',
